30de3b13e8597ab3b36b58fdf81e167773b1738f
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-05-27 18:34:32 +0900

    fix: add operation_segmentation to __init__.py #4

--- openpack_toolkit/codalab/__init__.py
+++ openpack_toolkit/codalab/__init__.py
@@ -1,6 +1,8 @@
+from . import operation_segmentation
 from .operation_segmentation import eval_operation_segmentation, eval_operation_segmentation_wrapper
 
 __all__ = [
+    "operation_segmentation",
     "eval_operation_segmentation",
     "eval_operation_segmentation_wrapper"
 ]
