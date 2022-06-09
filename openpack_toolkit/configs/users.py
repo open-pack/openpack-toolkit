af53380ab62ee46ab6d02ee444d78960e7e87f37
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-09 17:06:52 +0900

    fix: update user configs

---
+++ openpack_toolkit/configs/users.py
@@ -0,0 +1,157 @@
+from ._schema import SessionConfig, UserConfig
+
+U0102 = UserConfig(
+    id=102,
+    name="U0102",
+    sessions={
+        "S0100": SessionConfig(
+            duration="51m2s",
+            end="51m2s",
+            start="2021-10-22 11:19:17.749000",
+        ),
+        "S0200": SessionConfig(
+            duration="39m30s",
+            end="39m30s",
+            start="2021-10-22 12:31:04.594000",
+        ),
+        "S0300": SessionConfig(
+            duration="36m54s",
+            end="36m54s",
+            start="2021-10-22 14:14:37.613000",
+        ),
+        "S0400": SessionConfig(
+            duration="33m29s",
+            end="33m29s",
+            start="2021-10-22 15:08:15.639000",
+        ),
+        "S0500": SessionConfig(
+            duration="30m32s",
+            end="30m32s",
+            start="2021-10-22 15:56:21.597000",
+        ),
+    },
+)
+U0103 = UserConfig(
+    id=103,
+    name="U0103",
+    sessions={
+        "S0100": SessionConfig(
+            duration="20m25s",
+            end="20m25s",
+            start="2021-11-12 12:38:41.376000",
+        ),
+        "S0200": SessionConfig(
+            duration="36m4s",
+            end="36m4s",
+            start="2021-11-12 14:11:12.880000",
+        ),
+        "S0300": SessionConfig(
+            duration="34m15s",
+            end="34m15s",
+            start="2021-11-12 15:11:03.836000",
+        ),
+        "S0400": SessionConfig(
+            duration="32m2s",
+            end="32m2s",
+            start="2021-11-12 16:07:06.348000",
+        ),
+        "S0500": SessionConfig(
+            duration="30m22s",
+            end="30m22s",
+            start="2021-11-12 17:19:14.444000",
+        ),
+    },
+)
+U0105 = UserConfig(
+    id=105,
+    name="U0105",
+    sessions={
+        "S0100": SessionConfig(
+            duration="40m54s",
+            end="40m54s",
+            start="2021-11-19 11:20:19.080000",
+        ),
+        "S0200": SessionConfig(
+            duration="37m5s",
+            end="37m5s",
+            start="2021-11-19 12:19:27.026000",
+        ),
+        "S0300": SessionConfig(
+            duration="33m40s",
+            end="33m40s",
+            start="2021-11-19 14:04:13.586000",
+        ),
+        "S0400": SessionConfig(
+            duration="33m38s",
+            end="33m38s",
+            start="2021-11-19 14:59:09.378000",
+        ),
+        "S0500": SessionConfig(
+            duration="33m32s",
+            end="33m32s",
+            start="2021-11-19 15:51:11.856000",
+        ),
+    },
+)
+U0106 = UserConfig(
+    id=106,
+    name="U0106",
+    sessions={
+        "S0100": SessionConfig(
+            duration="49m15s",
+            end="49m15s",
+            start="2021-11-26 11:12:18.151000",
+        ),
+        "S0200": SessionConfig(
+            duration="35m11s",
+            end="35m11s",
+            start="2021-11-26 12:22:00.874000",
+        ),
+        "S0300": SessionConfig(
+            duration="34m34s",
+            end="34m34s",
+            start="2021-11-26 14:14:28.602000",
+        ),
+        "S0400": SessionConfig(
+            duration="36m27s",
+            end="36m27s",
+            start="2021-11-26 15:06:34.360000",
+        ),
+        "S0500": SessionConfig(
+            duration="17m12s",
+            end="17m12s",
+            start="2021-11-26 16:01:44.961000",
+        ),
+    },
+)
+U0107 = UserConfig(
+    id=107,
+    name="U0107",
+    sessions={
+        "S0100": SessionConfig(
+            duration="37m28s",
+            end="37m28s",
+            start="2021-12-03 11:10:05.656000",
+        ),
+        "S0200": SessionConfig(
+            duration="35m34s",
+            end="35m34s",
+            start="2021-12-03 12:07:50.691000",
+        ),
+        "S0300": SessionConfig(
+            duration="31m55s",
+            end="31m55s",
+            start="2021-12-03 13:52:41.219000",
+        ),
+        "S0400": SessionConfig(
+            duration="30m20s",
+            end="30m20s",
+            start="2021-12-03 14:43:09.336000",
+        ),
+        "S0500": SessionConfig(
+            duration="30m11s",
+            end="30m11s",
+            start="2021-12-03 15:32:26.547000",
+        ),
+    },
+)
