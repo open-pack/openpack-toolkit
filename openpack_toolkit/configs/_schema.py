d85fc14053fc0c50e2fa778e1fefc1b88813963e
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-10 00:09:31 +0900

    feat: add DataStream schema #6

--- openpack_toolkit/configs/_schema.py
+++ openpack_toolkit/configs/_schema.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List, Optional
 
 from omegaconf import MISSING
+
+# ======
+#  User
+# ======
 
 
 @dataclass
@@ -16,3 +20,48 @@ class UserConfig:
     id: int = MISSING
     name: str = MISSING
     sessions: Dict[str, SessionConfig] = MISSING  # DictConfig
+
+
+# =========
+#  Dataset
+# =========
+
+
+@dataclass
+class DataSplitConfig:
+    train: Optional[List[List]] = None
+    val: Optional[List[List]] = None
+    test: Optional[List[List]] = None
+    submission: Optional[List[List]] = None
+
+
+@dataclass
+class DataStreamConfig:
+    @dataclass
+    class Paths:
+        # path to the root directory of this stream.
+        rootdir: Optional[str] = None
+        template: Optional[str] = None
+
+    schema: str = MISSING
+    name: str = MISSING
+    description: Optional[str] = None
+    path: Paths = MISSING
+    frame_rate: int = MISSING  # [Hz, fps]
+
+
+@dataclass
+class ImuConfig(DataStreamConfig):
+    schema: str = "ImuConfig"
+    nodes: List[str] = MISSING
+    acc: bool = True
+    gyro: bool = True
+    quat: bool = True
+
+
+@dataclass
+class KeypointConfig(DataStreamConfig):
+    schema: str = "KeypointConfig"
+    category: str = MISSING
+    model: str = MISSING
+    nodes: Dict[int, str] = MISSING
--- tests/configs/test_schema.py
+++ tests/configs/test_schema.py
@@ -1,7 +1,14 @@
 import pytest
 from omegaconf import DictConfig, OmegaConf
 
-from openpack_toolkit.configs._schema import SessionConfig, UserConfig
+from openpack_toolkit.configs._schema import (
+    DataSplitConfig,
+    DataStreamConfig,
+    ImuConfig,
+    KeypointConfig,
+    SessionConfig,
+    UserConfig,
+)
 
 
 @pytest.fixture()
@@ -38,4 +45,65 @@ def test_UserConfig__01(sessions):
     conf = OmegaConf.structured(conf)
     print(OmegaConf.to_yaml(conf))
 
+    assert isinstance(conf, DictConfig)
+
+
+def test_DataSplitConfig__01():
+    conf = DataSplitConfig(
+        train=[
+            ["U0102", "S0100"],
+            ["U0102", "S0200"],
+            ["U0102", "S0300"],
+        ],
+        val=[
+            ["U0102", "S0400"],
+        ],
+        test=[
+            ["U0102", "S0500"],
+        ],
+        submission=[
+            ["U0102", "S0500"],
+        ]
+    )
+
+    conf = OmegaConf.structured(conf)
+    print(OmegaConf.to_yaml(conf))
+    assert isinstance(conf, DictConfig)
+
+    assert conf.train[0][0] == "U0102"
+    assert conf.train[0][1] == "S0100"
+
+
+def test_ImuConfig__01():
+    conf = ImuConfig(
+        name="atr-acc",
+        path=DataStreamConfig.Paths(
+            template="/foo/huga.csv"
+        ),
+        frame_rate=30,
+        nodes=["atr01", "atr02", "atr03", "atr04"],
+        acc=True,
+        gyro=False,
+        quat=False,
+    )
+
+    conf = OmegaConf.structured(conf)
+    print(OmegaConf.to_yaml(conf))
+    assert isinstance(conf, DictConfig)
+
+
+def test_KeypointConfig__01():
+    conf = KeypointConfig(
+        name="atr-acc",
+        path=DataStreamConfig.Paths(
+            template="/foo/huga.csv"
+        ),
+        frame_rate=30,
+        category="2d-kpt",
+        model="foo-hrnet",
+        nodes={0: "node0", 1: "node1"},
+    )
+
+    conf = OmegaConf.structured(conf)
+    print(OmegaConf.to_yaml(conf))
     assert isinstance(conf, DictConfig)
