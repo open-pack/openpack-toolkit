4655dadead44449a63c0e5cd7ee58e67d6407045
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-10 18:27:14 +0900

    fix: update test/confg/dataset

--- tests/configs/test_dataset__stream.py
+++ tests/configs/test_dataset__stream.py
@@ -5,9 +5,9 @@ from openpack_toolkit.configs import datasets
 
 
 @pytest.mark.parametrize("conf", (
-    datasets.streams.OPENPACK_ATR_ACC_STREAM,
-    datasets.streams.OPENPACK_ATR_QAGS_STREAM,
-    datasets.streams.OPENPACK_KINECT_2D_KPT_STREAM,
+    datasets.streams.ATR_ACC_STREAM,
+    datasets.streams.ATR_QAGS_STREAM,
+    datasets.streams.KINECT_2D_KPT_STREAM,
 ))
 def test_users__DataStreamConfigs__01(conf):
     print(OmegaConf.to_yaml(conf))
