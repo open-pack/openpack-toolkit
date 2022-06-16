a76ce00862739a22c4a4a0df08769c6ac798c314
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-17 00:35:16 +0900

    fix: buf of optk.utils.cleanup_dir #12

--- tests/utils/test_io.py
+++ tests/utils/test_io.py
@@ -13,3 +13,12 @@ def test_cleanup_dir__01(tmpdir):
 
     cleanup_dir(subdir, exclude="hydra")
     assert len(list(subdir.iterdir())) == 1
+
+
+def test_cleanup_dir__02(tmpdir):
+    """ Subdir does not exists."""
+    subdir = Path(tmpdir, "sub1")
+    cleanup_dir(subdir, exclude="hydra")
+
+    assert subdir.exists()
+    assert len(list(subdir.iterdir())) == 0
