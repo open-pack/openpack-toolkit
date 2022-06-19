9e8a6b18dd8fa7e18b064ca1f6132f4e9443010b
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-19 16:37:42 +0900

    Docs: Add dataset descriptions (#22)
    * fix: remove docs from gitignore #21
    
    * feat: update config (include meta data)
    
    * feat: generate USER.md from config #21
    
    * fix: typo
    
    * feat: update DataStreamConfig
    
    * feat: generate DATA_STREAM.md
    
    * docs: add user and data description #21
    
    * fix: add header/body separater to USER.md #21
    
    * feat: add activity class defs #21
    
    * fix: list style #21
    
    * feat: add name and description to DataSplitConfig #21
    
    * feat: add DataSplitConfig #21
    
    * fix: path #21
    
    * feat: add command to run after update of configs #21
    
    * feat: update cofig modules #21
    
    * feat: add docs to top README #21
    
    * fix: replace table with list
    
    * fix: updte docs (DATA_SPLIT.md)
    
    * fix: heading level

---
+++ configs/docs/gen_DATA_SPLIT.py
@@ -0,0 +1,45 @@
+"""Generate ./docs/DATA_SPLIT.md
+"""
+from logging import DEBUG, basicConfig, getLogger
+from pathlib import Path
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+basicConfig(level=DEBUG)
+logger = getLogger(__name__)
+
+TARGET_DATA_SPLIT = [
+    "debug",
+    "pilot-challenge",
+]
+
+
+def main():
+
+    splits = []
+    for split_name in TARGET_DATA_SPLIT:
+        path = Path(f"../dataset/split/{split_name}.yaml")
+        logger.info(f"load DataSplitConfig from {path}")
+        with open(path, "r") as f:
+            data = yaml.safe_load(f)
+        splits.append(data)
+
+    # build python script with jinja2
+    env = Environment(
+        loader=FileSystemLoader("./templates"),
+        trim_blocks=True
+    )
+
+    template = env.get_template("DATA_SPLIT.md.jinja")
+    code = template.render(splits=splits)
+
+    # write a python script
+    path = Path("../../docs/DATA_SPLIT.md")
+    logger.info(f"write docs/DATA_SPLIT.md to {path}")
+    with open(path, "w") as f:
+        f.write(code)
+
+
+if __name__ == "__main__":
+    main()
