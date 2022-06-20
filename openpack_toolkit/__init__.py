7b2210dc4eb03c10f1790eb1eaccb1fd40cbd99b
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-20 21:43:10 +0900

    feat: update package version to v0.3.0

--- openpack_toolkit/__init__.py
+++ openpack_toolkit/__init__.py
@@ -2,7 +2,7 @@
 .. include:: ../README.md
 """
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 from . import codalab, configs, data, utils
 from .activity import ActClass, ActSet
 from .configs.datasets.annotations import OPENPACK_OPERATIONS
