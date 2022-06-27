3f52f61e1ef7a64b4338e714cfe38f986d63a4e8
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-06-27 21:27:36 +0900

    feat-ht: add dataloader for scan log (HT)  (#34)
    * feat: add sample data of system #33
    
    * feat: add dataloader for scan log data #33

--- openpack_toolkit/data/dataloader.py
+++ openpack_toolkit/data/dataloader.py
@@ -34,6 +34,7 @@ def load_annotation(
         null_class_id = null_class_id[-1]
 
     df = pd.read_csv(path)
+    logger.debug(f"load annotation data from {path} -> df={df.shape}")
     ut_min, ut_max = df["unixtime"].min(), df["unixtime"].max()
 
     null_record = df.head(1).copy()
@@ -90,6 +91,7 @@ def load_keypoints(path: Path) -> Tuple[np.ndarray, np.ndarray]:
     """
     with open(path, "r") as f:
         data = json.load(f)
+    logger.debug(f"load keypoints from {path}")
 
     T, X = [], []
     for i, d in enumerate(data["annotations"][:]):
@@ -170,3 +172,34 @@ def load_imu(
 
     x_ret = np.concatenate(x_ret, axis=0)
     return ts_ret, x_ret
+
+
+def load_and_resample_scan_log(
+    path: Path,
+    unixtimes_ms: np.ndarray,
+) -> np.ndarray:
+    """Load scan log data such as HT, and make binary vector for given timestamps.
+    Elements that have the same timestamp in second precision are marked as 1.
+    Other values are set to 0.
+
+    Args:
+        path (Path): path to a scan log CSV file.
+        unixtimes_ms (np.ndarray):  unixtime seqeuence (milli-scond precision).
+            shape=(T,).
+
+    Returns:
+        np.ndarray: binary 1d vector.
+    """
+    assert unixtimes_ms.ndim == 1
+    df = pd.read_csv(path)
+    logger.info(f"load scan log from {path} -> df={df.shape}")
+
+    unixtimes_sec = unixtimes_ms // 1000
+
+    X_log = np.zeros(len(unixtimes_ms)).astype(np.int32)
+    for utime_ms in df["unixtime"].values:
+        utime_sec = utime_ms // 1000
+        ind = np.where(unixtimes_sec == utime_sec)[0]
+        X_log[ind] = 1
+
+    return X_log
