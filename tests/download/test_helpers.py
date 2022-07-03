218bb8b3420f1b6a78c6097e843a3d4b613c6319
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-07-04 00:11:25 +0900

    fix: unit-test of download function #35

--- tests/download/test_helpers.py
+++ tests/download/test_helpers.py
@@ -3,11 +3,13 @@ from pathlib import Path
 from openpack_toolkit.download._helpers import download_openpack_from_zenodo
 
 
-def test_download_openpack_from_zenodo__01():
-    rootdir = Path("../data/datasets")
+def test_download_openpack_from_zenodo__01(tmpdir):
+    rootdir = Path(tmpdir, "data", "datasets")
+    rootdir.mkdir(exist_ok=True, parents=True)
+
     streams = [
-        "atr-qags",
-        "kinect-2d-kpt",
+        # "atr-qags",
+        # "kinect-2d-kpt",
         "openpack-operations",
     ]
     version = "v0.2.0"
