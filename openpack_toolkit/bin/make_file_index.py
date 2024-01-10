9d67f7b40e289a3e75286afa60b19c31dbeeb4e0
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-01-11 01:21:11 +0900

    fix: add subcommand

--- openpack_toolkit/bin/make_file_index.py
+++ openpack_toolkit/bin/make_file_index.py
@@ -19,21 +19,28 @@ logger = getLogger(__name__)
 
 def make_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
+    subparsers = parser.add_subparsers(required=True)
+
+    # == Make File Index ==
+    parser_make_index = subparsers.add_parser(
+        "make-index", help="Make file index of the OpenPack dataset. see `add -h`"
+    )
+    parser_make_index.set_defaults(handler=entry_func_make_index)
+    parser_make_index.add_argument(
         "-r",
         "--rootdir",
         required=True,
         type=Path,
         help="OpenPack dataset directory.",
     )
-    parser.add_argument(
+    parser_make_index.add_argument(
         "-v",
         "--version",
         default=LATEST_VERSION_ON_ZENODO,
         type=str,
         help=f"dataset version. (Default: {LATEST_VERSION_ON_ZENODO})",
     )
-    parser.add_argument(
+    parser_make_index.add_argument(
         "--data-repo",
         default=DATA_REPO_ZENODO,
         type=str,
@@ -42,7 +49,7 @@ def make_parser():
             f"Option: {DATA_REPO_ZENODO}, {DATA_REPO_GOOGLE_DRIVE}, {DATA_REPO_GOOGLE_DRIVE_RGB})"
         ),
     )
-    parser.add_argument(
+    parser_make_index.add_argument(
         "--output-dir",
         required=False,
         default=DEFAULT_OUTPUT_DIR,
@@ -52,11 +59,7 @@ def make_parser():
     return parser
 
 
-def entry_func():
-    parser = make_parser()
-    args = parser.parse_args()
-    print(args)
-
+def entry_func_make_index(args: argparse.Namespace):
     df = make_dataset_file_index(args.rootdir, args.version)
     logger.info(f"File Index:\n{df}")
 
@@ -64,6 +67,16 @@ def entry_func():
     logger.info(f"Save file index to {path}")
     path.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(path, index=False)
+
+
+def entry_func():
+    parser = make_parser()
+    args = parser.parse_args()
+
+    if hasattr(args, "handler"):
+        args.handler(args)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
