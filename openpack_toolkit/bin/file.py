fd2e1ea2a8db2990a67c8b84aa5c84356ca12172
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-01-12 00:25:18 +0900

    fix: add check command

--- openpack_toolkit/bin/file.py
+++ openpack_toolkit/bin/file.py
@@ -3,21 +3,50 @@ from logging import INFO, basicConfig, getLogger
 from pathlib import Path
 
 from openpack_toolkit import DATASET_VERSION
-from openpack_toolkit.validation.file import make_dataset_file_index
-
-LATEST_VERSION_ON_ZENODO = DATASET_VERSION
-DEFAULT_OUTPUT_DIR = Path(".")
-
-DATA_REPO_ZENODO = "zenodo"
-DATA_REPO_GOOGLE_DRIVE = "GoogleDrive"
-DATA_REPO_GOOGLE_DRIVE_RGB = "GoogleDriveRGB"
-
+from openpack_toolkit.validation.file import (
+    FILE_EXISTS_KEY_NAME,
+    DatasetRepo,
+    DatasetStatus,
+    check_files_exists,
+    get_dataset_file_index_uri,
+    make_dataset_file_index,
+)
 
 basicConfig(level=INFO)
 logger = getLogger(__name__)
 
+LATEST_VERSION_ON_ZENODO = DATASET_VERSION
+DEFAULT_OUTPUT_DIR = Path(".")
+
 
 def make_parser():
+    def _add_common_params(parser: argparse.ArgumentParser):
+        parser.add_argument(
+            "-r",
+            "--rootdir",
+            required=True,
+            type=Path,
+            help="OpenPack dataset directory.",
+        )
+        parser.add_argument(
+            "-v",
+            "--version",
+            default=LATEST_VERSION_ON_ZENODO,
+            type=str,
+            help=f"dataset version. (Default: {LATEST_VERSION_ON_ZENODO})",
+        )
+        parser.add_argument(
+            "--data-repo",
+            default=DatasetRepo.ZENODO.value,
+            type=str,
+            help=(
+                f"dataset version. (Default: {DatasetRepo.ZENODO.value}, "
+                f"Option: {DatasetRepo.ZENODO.value}, {DatasetRepo.GOOGLE_DRIVE.value}, "
+                f"{DatasetRepo.GOOGLE_DRIVE_RGB.value})"
+            ),
+        )
+        return parser
+
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(required=True)
 
@@ -26,30 +55,20 @@ def make_parser():
         "make-index", help="Make file index of the OpenPack dataset. See `make-index -h`"
     )
     parser_make_index.set_defaults(handler=entry_func_make_index)
+    parser_make_index = _add_common_params(parser_make_index)
     parser_make_index.add_argument(
-        "-r",
-        "--rootdir",
-        required=True,
+        "--output-dir",
+        required=False,
+        default=DEFAULT_OUTPUT_DIR,
         type=Path,
-        help="OpenPack dataset directory.",
+        help=f"output directory. (Default: {DEFAULT_OUTPUT_DIR})",
     )
-    parser_make_index.add_argument(
-        "-v",
-        "--version",
-        default=LATEST_VERSION_ON_ZENODO,
-        type=str,
-        help=f"dataset version. (Default: {LATEST_VERSION_ON_ZENODO})",
-    )
-    parser_make_index.add_argument(
-        "--data-repo",
-        default=DATA_REPO_ZENODO,
-        type=str,
-        help=(
-            f"dataset version. (Default: {DATA_REPO_ZENODO}, "
-            f"Option: {DATA_REPO_ZENODO}, {DATA_REPO_GOOGLE_DRIVE}, {DATA_REPO_GOOGLE_DRIVE_RGB})"
-        ),
-    )
-    parser_make_index.add_argument(
+
+    # == Check Files ==
+    parser_check = subparsers.add_parser("check", help="Check downloaded files. See `check -h`")
+    parser_check.set_defaults(handler=entry_func_check)
+    parser_check = _add_common_params(parser_check)
+    parser_check.add_argument(
         "--output-dir",
         required=False,
         default=DEFAULT_OUTPUT_DIR,
@@ -67,6 +86,29 @@ def entry_func_make_index(args: argparse.Namespace):
     logger.info(f"Save file index to {path}")
     path.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(path, index=False)
+
+
+def entry_func_check(args: argparse.Namespace):
+    file_index_uri = get_dataset_file_index_uri(args.version, args.data_repo)
+    logger.info(f"Get file index from {file_index_uri}")
+
+    status_code, df_file_index = check_files_exists(args.rootdir, args.version, file_index_uri)
+
+    path = Path(
+        args.output_dir, f"file_index_OpenPack_{args.version}_{args.data_repo}_check_result.csv"
+    )
+    logger.info(f"Save check results to {path}")
+    path.parent.mkdir(parents=True, exist_ok=True)
+    df_file_index.to_csv(path, index=False)
+
+    if status_code == DatasetStatus.HAS_MISSING_FILE:
+        df_missing_files = df_file_index[df_file_index[FILE_EXISTS_KEY_NAME] == False]
+        logger.warning(
+            f"{len(df_missing_files)} files ({len(df_missing_files)/len(df_file_index)*100:.1f}%)"
+            f" are missing.\n{df_missing_files}"
+        )
+    else:
+        logger.info("No missing files! It's ready to use!")
 
 
 def entry_func():
--- openpack_toolkit/validation/file.py
+++ openpack_toolkit/validation/file.py
@@ -1,9 +1,11 @@
 import datetime
+from enum import Enum
 from logging import getLogger
 from pathlib import Path
 
-
 import pandas as pd
+
+from openpack_toolkit import DATASET_VERSION
 
 logger = getLogger(__name__)
 
@@ -13,6 +15,51 @@ ST_CTIME_KEY_NAME = "st_ctime"  # Time of most recent metadata change expressed 
 ST_SIZE_KEY_NAME = "size_bytes"
 ST_SIZE_HUMAN_READABLE_KEY_NAME = "size"
 IS_DIR_KEY_NAME = "is_dir"
+FILE_EXISTS_KEY_NAME = "file_exists"
+
+LATEST_VERSION_ON_ZENODO = DATASET_VERSION
+
+
+class DatasetRepo(Enum):
+    ZENODO = "zenodo"
+    GOOGLE_DRIVE = "GoogleDrive"
+    GOOGLE_DRIVE_RGB = "GoogleDriveRGB"
+
+    @classmethod
+    def get_names(cls) -> list:
+        return [i.name for i in cls]
+
+    @classmethod
+    def get_values(cls) -> list:
+        return [i.value for i in cls]
+
+
+class DatasetVersion(Enum):
+    VERSION_0_3_0 = "v0.3.0"
+    VERSION_1_0_0 = "v1.0.0"
+
+    @classmethod
+    def get_names(cls) -> list:
+        return [i.name for i in cls]
+
+    @classmethod
+    def get_values(cls) -> list:
+        return [i.value for i in cls]
+
+
+class DatasetStatus(Enum):
+    OK = 0
+    DATASET_DIR_DOES_NOT_EXISTS = 1
+    HAS_MISSING_FILE = 2
+
+
+# TODO: How should I manage file indexes?
+OPENPACK_DATASET_INDEX_LIST: dict[tuple[str, str], str] = {
+    (
+        DatasetVersion.VERSION_1_0_0.value,
+        DatasetRepo.ZENODO.value,
+    ): "https://raw.githubusercontent.com/open-pack/openpack-dataset/main/release/v1.0.0/file_index_OpenPack_v1.0.0_zenodo.csv",
+}
 
 
 def path_bfs(path: Path, results: list) -> list:
@@ -49,7 +96,7 @@ def make_dataset_file_index(
     openpack_rootdir: Path,
     dataset_version: str,
     return_full_metrics: bool = False,
-):
+) -> pd.DataFrame:
     rootdir = Path(openpack_rootdir, dataset_version)
     if not rootdir.exists():
         raise FileNotFoundError(f"Dataset directory does not exists: {rootdir}")
@@ -92,3 +139,48 @@ def make_dataset_file_index(
         ]
         df = df[cols]
     return df
+
+
+def get_dataset_file_index_uri(
+    version: str = LATEST_VERSION_ON_ZENODO,
+    data_repo: str = DatasetRepo.ZENODO.value,
+):
+    if version not in DatasetVersion.get_values():
+        raise ValueError(f"Invalid dataset version: {version}")
+    if data_repo not in DatasetRepo.get_values():
+        raise ValueError(f"Invalid data repository name: {data_repo}")
+
+    key = (version, data_repo)
+    uri = OPENPACK_DATASET_INDEX_LIST.get(key)
+    if uri is None:
+        raise NotImplementedError(f"{version}@{data_repo} is not supported yet.")
+
+    return uri
+
+
+def check_files_exists(
+    openpack_rootdir: Path,
+    openpack_version: str,
+    file_index_uri: str,
+) -> tuple[DatasetStatus, pd.DataFrame]:
+    df_file_index = pd.read_csv(file_index_uri)
+    logger.info(f"Loading of file index was success. {len(df_file_index)} files should be there.")
+
+    rootdir = Path(openpack_rootdir, openpack_version)
+    if not rootdir.exists():
+        raise FileNotFoundError(f"Dataset directory does not exists: {rootdir}")
+
+    # Check file one by one
+    logger.info(f"Check files under {Path(openpack_rootdir, openpack_version)} ...")
+    df_file_index[FILE_EXISTS_KEY_NAME] = None
+    for idx, row in df_file_index.iterrows():
+        path = Path(openpack_rootdir, openpack_version, row[PATH_KEY_NAME])
+        df_file_index.loc[idx, FILE_EXISTS_KEY_NAME] = True if path.exists() else False
+
+    missing_files = df_file_index[df_file_index[FILE_EXISTS_KEY_NAME] == False]
+    if len(missing_files) == 0:
+        status_code = DatasetStatus.OK
+    else:
+        status_code = DatasetStatus.HAS_MISSING_FILE
+
+    return status_code, df_file_index
